01d468caa575eced130486bbfd6f213011e0d3e7
Michał Damięcki <michal.damiecki@codilime.com>
2019-01-17 07:40:48 +0100

    Added formik to NumberField.

--- src/Form/fields/NumberField.tsx
+++ src/Form/fields/NumberField.tsx
@@ -1,34 +1,47 @@
+import {ErrorMessage, Field} from 'formik';
 import * as React from 'react';
 
 import {IWidget} from '../../typings/IWidget';
+import Errors from '../components/Errors';
+import validator from '../Validator';
 import InputWidget from '../widgets/InputWidget';
 
 export class NumberField extends React.Component<IWidget> {
-  private field = undefined;
-
-  public get value(): number | null | undefined {
-    return this.field.value;
-  }
-
-  public get isValid(): boolean {
-    return this.field.isValid;
-  }
-
   public render(): React.ReactNode {
-    const {schema, uiSchema, isRequired, value} = this.props;
+    const {schema, uiSchema, id} = this.props;
 
     return (
-      <InputWidget
-        ref={c => {
-          this.field = c;
-        }}
-        schema={schema}
-        uiSchema={uiSchema}
-        isRequired={isRequired}
-        value={value}
-      />
+      <React.Fragment>
+        <ErrorMessage name={id} />
+        <Field
+          name={id}
+          schema={schema}
+          uiSchema={uiSchema}
+          validate={this.validate}
+          component={InputWidget}
+        />
+      </React.Fragment>
     );
   }
+
+  private validate = value => {
+    const {isRequired, schema} = this.props;
+    const errors = [];
+
+    if (isRequired && !value) {
+      errors.push({message: 'Required'});
+    }
+
+    if (value !== undefined) {
+      validator.validate(schema, value);
+    }
+
+    if (validator.errors) {
+      errors.push(...validator.errors);
+    }
+
+    return errors.length > 0 ? <Errors errors={errors} /> : undefined;
+  };
 }
 
 export default NumberField;
