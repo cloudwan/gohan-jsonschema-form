cdc9f664acf329f2c98b6378fb1b2556a3b44ef2
Michał Damięcki <michal.damiecki@codilime.com>
2019-01-17 07:59:43 +0100

    Added formik to ArrayField.

--- src/Form/fields/ArrayField/components/List.tsx
+++ src/Form/fields/ArrayField/components/List.tsx
@@ -1,4 +1,11 @@
+import {FieldArrayRenderProps, getIn} from 'formik';
+import {JSONSchema4} from 'json-schema';
 import * as React from 'react';
+
+import {IWidget} from '../../../../typings/IWidget';
+import SchemaField from '../../../fields/SchemaField';
+import ListAddButton from './ListAddButton';
+import ListItem from './ListItem';
 
 import styles from './List.css';
 
@@ -6,8 +13,49 @@ interface TListProps {
   children: React.ReactNode;
 }
 
-const List = ({children}: TListProps): JSX.Element => (
-  <ul className={styles.listContainer}>{children}</ul>
-);
+class List extends React.Component<FieldArrayRenderProps & IWidget> {
+  public render() {
+    const {form, name, schema} = this.props;
+
+    const fieldValue = getIn(form.values, name);
+
+    return (
+      <React.Fragment>
+        <ListAddButton onClick={this.handleAddClick} />
+        <ul className={styles.listContainer}>
+          {fieldValue &&
+            fieldValue.map((value, index) => (
+              <ListItem
+                index={index}
+                key={`${name}.${index}`}
+                itemsCount={fieldValue.length}
+                onMoveUpButtonClick={this.handleReorderClick(index, index - 1)}
+                onMoveDownButtonClick={this.handleReorderClick(
+                  index,
+                  index + 1,
+                )}
+                onRemoveButtonClick={this.handleRemoveClick(index)}
+              >
+                <SchemaField id={`${name}.${index}`} schema={schema.items} />
+              </ListItem>
+            ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  private handleAddClick = (): void => {
+    const items = this.props.schema.items as JSONSchema4;
+    this.props.push(items.default);
+  };
+
+  private handleReorderClick = (indexA: number, indexB: number) => () => {
+    this.props.swap(indexA, indexB);
+  };
+
+  private handleRemoveClick = (index: number) => () => {
+    this.props.remove(index);
+  };
+}
 
 export default List;
