95e8abf2531d635575293edcf5fe6cf6e498b502
Michał Damięcki <michal.damiecki@codilime.com>
2019-01-22 14:53:31 +0100

    Added formik to DateTimeWidget.

--- src/Form/widgets/DateTimeWidget/components/DateTimePicker.test.tsx
+++ src/Form/widgets/DateTimeWidget/components/DateTimePicker.test.tsx
@@ -9,9 +9,54 @@ chai.should();
 chai.use(chaiEnzyme());
 
 describe('DateTimePicker', () => {
+  const formikFormProps = {
+    dirty: false,
+    errors: {},
+    handleBlur: () => null,
+    handleChange: () => null,
+    handleReset: () => null,
+    handleSubmit: () => null,
+    initialValues: {},
+    isSubmitting: false,
+    isValid: false,
+    isValidating: false,
+    registerField: () => null,
+    resetForm: () => null,
+    setError: () => null,
+    setErrors: () => null,
+    setFieldError: () => null,
+    setFieldTouched: () => null,
+    setFieldValue: () => null,
+    setFormikState: () => null,
+    setStatus: () => null,
+    setSubmitting: () => null,
+    setTouched: () => null,
+    setValues: () => null,
+    submitCount: 0,
+    submitForm: () => null,
+    touched: {},
+    unregisterField: () => null,
+    validateField: () => null,
+    validateForm: () => null,
+    validateOnBlur: true,
+    validateOnChange: true,
+    values: {},
+  };
   it('should match snapshot for minimal number of props', () => {
     const wrapper = shallow(
-      <DateTimePicker onChange={() => null} onOk={() => null} />,
+      <DateTimePicker
+        schema={{
+          type: ['string'],
+          format: 'date-time',
+        }}
+        form={formikFormProps}
+        field={{
+          value: undefined,
+          onChange: () => null,
+          onBlur: () => null,
+          name: 'foo',
+        }}
+      />,
     );
 
     wrapper.should.to.matchSnapshot();
